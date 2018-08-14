c2c34146c3165b84dc33db11b3f9eb815c8d276e
dby521103 <596963475@qq.com>
2018-08-14 17:48:11 +0800

    09点45分

--- singleton/src/main/java/proxy/ProxyTest.java
+++ singleton/src/main/java/proxy/ProxyTest.java
@@ -8,14 +8,14 @@ public class ProxyTest {
     public static void main(String[] args) {
 
         //初始化被代理类
-        Poson f=new Kevin();
+        Kevin kevin=new Kevin();
 
         //初始化代理类
-        ProxyHander hander = new ProxyHander(f);
+        ProxyHander hander = new ProxyHander(kevin);
 
-        Class<?> taber = f.getClass();
+        Class<?> taber = kevin.getClass();
 
-        //通过jdk Proxy实现字节码重组 动态生成被代理类的接口实现
+        //通过jdk Proxy实现字节码重组 生成一个class文件 动态生成被代理类的接口实现
         Poson proxy = (Poson)Proxy.newProxyInstance(taber.getClassLoader(),taber.getInterfaces(),hander);
 
         proxy.findWork();
