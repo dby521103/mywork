bc1df1e1af0a87e947eb6abceaedda3798b1416b
dby521103 <596963475@qq.com>
2018-08-14 15:19:33 +0800

    09点45分

--- singleton/src/main/java/proxy/ProxyHander.java
+++ singleton/src/main/java/proxy/ProxyHander.java
@@ -5,30 +5,18 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
- * 实现动态代理
+ * 实现动态代理 实现 InvocationHandler
  * 被代理类必须是接口
  * 才能实现Proxy的字节流实现生存class文件
  */
 public class ProxyHander implements InvocationHandler {
 
-    //     * 获取被代理对象引用
+    //获取被代理对象引用
     private  Object ob;
 
     public ProxyHander(Object o) {
         ob =  o;
     }
-
-//    /**
-//     * 获取被代理对象引用
-//     * @param ob
-//     * @return
-//     */
-//    public  Object getIntence(Object ob)
-//    {
-//         ob = ob;
-//
-//         return ob;
-//    }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
@@ -37,7 +25,7 @@ public class ProxyHander implements InvocationHandler {
         System.out.println("开始代理：请问需要什么？");
 
         method.invoke(ob,args);
-        System.out.println("稍等,请你先登记下");
+        System.out.println("稍等,请先登记下");
         return null;
     }
 }
